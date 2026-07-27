97f4c1fc2023186fc4fc592faa0bf2b36ab5892a
fishbonecactus <crocolson@gmail.com>
2026-07-28 01:17:21 +0900

    Add files via upload

--- update.py
+++ update.py
@@ -5,6 +5,7 @@ BASE=os.path.dirname(os.path.abspath(__file__))
 def run(file):
     print(f"\n[{file} 실행]")
     r=subprocess.run([sys.executable,os.path.join(BASE,file)])
+
     if r.returncode:
         print(f"{file} 오류 발생")
         exit(1)
@@ -19,7 +20,9 @@ def main():
 
     print()
     print("전체 업데이트 완료")
-    input("엔터를 누르면 종료합니다.")
+
+    if sys.stdin.isatty():
+        input("엔터를 누르면 종료합니다.")
 
 if __name__=="__main__":
     main()
